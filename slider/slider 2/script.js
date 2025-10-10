4ba5f64b2f22d355ec31155762d5013dc15eb814
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-10-10 10:35:42 +0530

    Image Slider

---
+++ slider/slider 2/script.js
@@ -0,0 +1,28 @@
+let swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+
+  autoplay: {
+      delay: 3000,
+      disableOnIntraction: false,
+  },
+
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
